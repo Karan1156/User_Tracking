d64498dce5477ee857461304e9055192999f70e6
Karan <121567114+Karan1156@users.noreply.github.com>
2025-08-27 08:03:26 +0530

    Update apps.py

--- app/apps.py
+++ app/apps.py
@@ -4,3 +4,16 @@ from django.apps import AppConfig
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
+
+
+
+
+# core/apps.py
+from django.apps import AppConfig
+
+class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app'
+
+    def ready(self):
+        from . import scheduler
