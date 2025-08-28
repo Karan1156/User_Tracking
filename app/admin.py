c3a42b1963e8e16341b328a8dc8ba98dd7c56811
Karan <121567114+Karan1156@users.noreply.github.com>
2025-08-28 17:01:06 +0530

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Visitor,Blog
+from .models import Visitor,Blog,User
 # Register your models here.
+admin.register(User)
 admin.register(Visitor)
+
 admin.register(Blog)
